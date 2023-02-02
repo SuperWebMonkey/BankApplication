b25449d5a4e040e9921e8edf6c8194a36c17349d
SuperWebMonkey <ceduart828@student.glendale.edu>
2023-02-02 12:56:08 -0800

    mybatis editing changes

--- src/main/java/db/dao/IFlightDAO.java
+++ src/main/java/db/dao/IFlightDAO.java
@@ -3,5 +3,5 @@ package db.dao;
 import db.models.Flight;
 
 public interface IFlightDAO extends IBaseDAO<Flight> {
-    Flight getFlightByPrice(double db_price);
+    Flight getFlightByPrice(double price);
 }
